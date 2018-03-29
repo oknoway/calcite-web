8bc41cb681235d79d55dc84d32a40d5ba8c37b69
Nate Bedortha <nate@oknoway.com>
2018-03-29 16:06:23 -0700

    Rough stats page

--- docs/acetate.config.js
+++ docs/acetate.config.js
@@ -1,19 +1,26 @@
-var svg = require('./source/data/svg-icon.js');
-var font = require('./source/data/icon-font.js');
-var icons = require('./source/data/icon-social.js');
-var colors = require('./source/data/colors.js');
-var repo = require('./source/data/repo.js');
+var svg = require("./source/data/svg-icon.js");
+var font = require("./source/data/icon-font.js");
+var icons = require("./source/data/icon-social.js");
+var colors = require("./source/data/colors.js");
+var repo = require("./source/data/repo.js");
 
-module.exports = function (acetate) {
-  acetate.load('**/*.+(md|html)');
-  acetate.layout('documentation/**/*', 'layouts/_doc');
-  acetate.layout('examples/**/*', 'layouts/_examples');
-  acetate.data('table_of_contents', 'table_of_contents.yml');
-  acetate.data('stats', 'data/stats.json');
-  acetate.data('pkg', '../../package.json');
-  acetate.data('colors', colors);
-  acetate.data('icons', icons);
-  acetate.data('font', font);
-  acetate.data('svg', svg);
-  acetate.data('repo', repo);
+module.exports = function(acetate) {
+  acetate.load("**/*.+(md|html)");
+  acetate.layout("documentation/**/*", "layouts/_doc");
+  acetate.layout("examples/**/*", "layouts/_examples");
+  acetate.data("table_of_contents", "table_of_contents.yml");
+  acetate.data("stats", "data/stats.json");
+  acetate.data("pkg", "../../package.json");
+  acetate.data("colors", colors);
+  acetate.data("icons", icons);
+  acetate.data("font", font);
+  acetate.data("svg", svg);
+  acetate.data("repo", repo);
+
+  /**
+   * Custom filter to stringify a JSON object
+   */
+  acetate.filter("stringify", function(object) {
+    return JSON.stringify(object);
+  });
 };
